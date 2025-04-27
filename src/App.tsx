26e8875697c676ebb88da41489fd74bfc9d39bcb
takitajwar17 <tajwaruzzaman@iut-dhaka.edu>
2025-04-28 04:45:52 +0600

    Add Vercel Analytics integration to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { ThemeProvider } from "./ThemeContext";
 import AppContent from "./AppContent";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   return (
     <ThemeProvider>
       <AppContent />
+      <Analytics />
     </ThemeProvider>
   );
 }
